3ce5a25588d313df461d0a31b8d63ca3de821aaf
jay chacko <jaychackoui@gmail.com>
2017-10-02 00:23:08 -0500

    added models

---
+++ src/comicbookgal/Models/CominBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comicbookgal.Models
+{
+    public class CominBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+
+    }
+}
